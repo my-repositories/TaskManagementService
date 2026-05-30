1178b83d7225b6bef5046e89d1f862a1d7642547
Aleksandr Loktionov <loktionov129@gmail.com>
2026-05-30 13:39:52 +0700

    feat(api): add swagger

--- src/TaskManagementService.Api/Program.cs
+++ src/TaskManagementService.Api/Program.cs
@@ -3,8 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -12,6 +15,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
